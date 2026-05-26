f2692ae15ef0782eff0e9eb1bada5bbe27179b50
Ranyák Roland Adalbert <r.rol2005@yahoo.com>
2026-05-26 12:19:51 +0200

    fix(auth): remove unused nuxtapp parameter to fix strict typecheck

--- app/plugins/auth.ts
+++ app/plugins/auth.ts
@@ -1,4 +1,4 @@
-export default defineNuxtPlugin(async (nuxtApp) => {
+export default defineNuxtPlugin(async () => {
   const { fetchUser } = useAuth();
   
   // Only fetch user on the server (SSR) or if it's a client-side navigation and we don't have state
