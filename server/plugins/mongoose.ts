58eaa254671757da77577397074116fd2ec4989d
Ranyák Roland Adalbert <r.rol2005@yahoo.com>
2026-05-26 19:39:53 +0200

    build: mongoose if

--- server/plugins/mongoose.ts
+++ server/plugins/mongoose.ts
@@ -1,9 +1,11 @@
 import { connectDb } from '../utils/db';
 
 export default defineNitroPlugin(async () => {
+  if (import.meta.prerender) return;
+
   const config = useRuntimeConfig();
 
-  if (!config.mongodbUri) return;
+  if (!config.mongodbUri) throw new Error('MONGODB_URI is not set');
 
   await connectDb();
 });
